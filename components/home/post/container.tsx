3cd30aea93bc0993a5ad431d6a4e5154b65c4890
joel <joelaugusto>
2022-03-02 01:25:28 +0200

    fix: fixed scroll bar width

--- components/home/post/container.tsx
+++ components/home/post/container.tsx
@@ -6,7 +6,42 @@ const PostContainer = () => {
   return (
     <div className="mx-6 my-20">
       <h1 className="my-6 text-3xl">Postagens</h1>
-      <div className="mt-10 grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+      <div className="mt-10 grid gap-6 md:grid-cols-3 lg:grid-cols-4">
+        <Post
+          title="Rotação de Culturas"
+          views="53K visualizações"
+          date="2 semanas atrás"
+          userImage="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+          postBg="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+        />
+        <Post
+          title="Rotação de Culturas"
+          views="53K visualizações"
+          date="2 semanas atrás"
+          userImage="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+          postBg="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+        />
+        <Post
+          title="Rotação de Culturas"
+          views="53K visualizações"
+          date="2 semanas atrás"
+          userImage="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+          postBg="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+        />
+        <Post
+          title="Rotação de Culturas"
+          views="53K visualizações"
+          date="2 semanas atrás"
+          userImage="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+          postBg="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+        />
+        <Post
+          title="Rotação de Culturas"
+          views="53K visualizações"
+          date="2 semanas atrás"
+          userImage="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+          postBg="https://avatar.oxro.io/avatar.svg?name=Joel+Augusto"
+        />
         <Post
           title="Rotação de Culturas"
           views="53K visualizações"
