f8e8c029bbf9d80245543bc0f9a8bac7791792d3
joel <joelaugusto>
2022-03-11 02:38:52 +0200

    fix: scroll on home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,30 +23,30 @@ const Home: NextPage = (props:any) => {
   
 
   return (
-    <div>
-      <div className="flex">
-        <Head>
-          <title>Início</title>
-          <meta name="Início" content="Página Inicial" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <div className="hidden h-screen w-2/5 border-r bg-white md:flex md:w-1/4">
-          <div className="mx-auto py-10">
-            <h1 className="mb-10 cursor-pointer text-2xl font-bold text-emerald-400 duration-150">
-              GCSA
-            </h1>
-            <LeftSidebar />
-          </div>
+    <div className="flex">
+      <Head>
+        <title>Início</title>
+        <meta name="Início" content="Página Inicial" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="hidden h-screen w-2/5 border-r bg-white md:flex md:w-1/4">
+        <div className="mx-auto py-10">
+          <h1 className="mb-10 cursor-pointer text-2xl font-bold text-emerald-400 duration-150">
+            GCSA
+          </h1>
+          <LeftSidebar />
         </div>
-        <main className="min-h-screen w-full bg-white">
-          <Navbar user={props.user} onSearch={onSearch}/>
-          <AdsContainer />
-          <PostContainer posts={posts}/>
-        </main>
       </div>
-      <footer className="w-full text-center py-6 text">
-        <p>Gestão de Cadeia de suplementos agricolas</p>
-      </footer>
+      <main className="min-h-screen w-full bg-white">
+        <Navbar user={props.user} onSearch={onSearch} />
+        <div className="h-[calc(100vh-240px)] overflow-auto">
+          <AdsContainer />
+          <PostContainer posts={posts} />
+          <footer className="text w-full py-6 text-center">
+            <p>Gestão de Cadeia de suplementos agricolas</p>
+          </footer>
+        </div>
+      </main>
     </div>
   )
 }
