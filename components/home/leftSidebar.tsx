e36f72b8fb701dae4523b46718a8b6f834f80bc4
joel <joelaugusto>
2022-03-22 01:34:21 +0200

    fix: supply chain link added at home page

--- components/home/leftSidebar.tsx
+++ components/home/leftSidebar.tsx
@@ -1,17 +1,29 @@
 import SideButton from "./SideButton";
-import { FiSettings, FiInfo, FiUser } from 'react-icons/fi'
-
+import { FiSettings, FiInfo, FiUser, FiMapPin } from 'react-icons/fi'
 import { BiMessageAltDetail } from 'react-icons/bi'
 
 import { AiOutlineHome } from 'react-icons/ai'
 
 const LeftSidebar = () => (
   <div>
-    <SideButton icon={<AiOutlineHome size={26} />} text="Início" to="/"/>
-    <SideButton icon={<FiInfo size={26} />} text="Anúncios" to="/"/>
-    <SideButton icon={<BiMessageAltDetail size={26} />} text="Mensagens" to="/messages"/>
-    <SideButton icon={<FiUser size={26} />} text="Perfil" to="/account"/>
-    <SideButton icon={<FiSettings size={26} />} text="Definições" to="/settings"/>
+    <SideButton icon={<AiOutlineHome size={26} />} text="Início" to="/" />
+    {/* <SideButton icon={<FiInfo size={26} />} text="Anúncios" to="/" /> */}
+    <SideButton
+      icon={<BiMessageAltDetail size={26} />}
+      text="Mensagens"
+      to="/messages"
+    />
+    <SideButton
+      icon={<FiMapPin size={26} />}
+      text="Cadeia de suplementos"
+      to="/supply-chain"
+    />
+    <SideButton icon={<FiUser size={26} />} text="Perfil" to="/account" />
+    <SideButton
+      icon={<FiSettings size={26} />}
+      text="Definições"
+      to="/settings"
+    />
   </div>
 )
 
