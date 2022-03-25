64b7c46dfe32f66547e425e4c4f5838645c6973a
joel <joelaugusto>
2022-03-25 02:47:48 +0200

    feat: enviroment variable added

--- utils/api.ts
+++ utils/api.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8080/api/v1',
   timeout: 30000,
   headers: {
-    'Accept': 'application/json',
+    Accept: 'application/json',
     'Content-Type': 'application/json',
   },
 })
