481f530c81008ccb1fe9f65e27563b3c06b1aa54
joel <joelaugusto97@gmail.com>
2022-09-24 16:01:10 +0200

    fix: fix home icons size

--- components/home/leftSidebar.tsx
+++ components/home/leftSidebar.tsx
@@ -5,26 +5,26 @@ import { AiOutlineHome } from 'react-icons/ai'
 
 const LeftSidebar = () => (
   <div>
-    <SideButton icon={<AiOutlineHome size={26} />} text="Início" to="/" />
-    {/* <SideButton icon={<FiInfo size={26} />} text="Anúncios" to="/" /> */}
+    <SideButton icon={<AiOutlineHome size={20} />} text="Início" to="/" />
+    {/* <SideButton icon={<FiInfo size={20} />} text="Anúncios" to="/" /> */}
     <SideButton
-      icon={<FiMail size={26} />}
+      icon={<FiMail size={20} />}
       text="Mensagens"
       to="/messages"
     />
     <SideButton
-      icon={<FiMapPin size={26} />}
+      icon={<FiMapPin size={20} />}
       text="Cadeia de suplementos"
       to="/supply-chain"
     />
     <SideButton
-      icon={<FiUsers size={26} />}
+      icon={<FiUsers size={20} />}
       text="Grupos"
       to="/groups"
     />
-    <SideButton icon={<FiUser size={26} />} text="Perfil" to="/account" />
+    <SideButton icon={<FiUser size={20} />} text="Perfil" to="/account" />
     <SideButton
-      icon={<FiSettings size={26} />}
+      icon={<FiSettings size={20} />}
       text="Definições"
       to="/settings"
     />
