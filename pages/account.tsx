7d70292ce68ed2a6b5b530eaaa0207f4f4034ff4
joel <joelaugusto97@gmail.com>
2022-10-04 03:56:51 +0200

    feat: add posts to profile page

--- pages/account.tsx
+++ pages/account.tsx
@@ -2,7 +2,10 @@ import { GetServerSidePropsContext, NextPage } from "next";
 import { useState} from 'react'
 import { ProfilePhotoUploader } from "../components/global/ProfilePhotoUploader";
 import HomeContainer from "../components/home/HomeContainer";
+import PostContainer from "../components/home/post/container";
+import Post from "../components/home/post/post";
 import api from "../utils/api";
+import dateUtil from "../utils/dateUtil";
 import session from "../utils/session";
 
 const Settings: NextPage = (props: any) => {
@@ -30,12 +33,45 @@ const Settings: NextPage = (props: any) => {
             <p>Nome: {props.user.name}</p>
             <p>Função: {props.user.role}</p>
             <p>Celular: {props.user.phone}</p>
+            <p>Biografia: {}</p>
           </div>
           <div className="col-span-1">
             <ProfilePhotoUploader
               setImages={setPictures}
               profilePhoto={profilePhoto}
             />
+            <div className="p-4">
+              <p>Minhas Postagens</p>
+              <div className="mt-4 grid gap-6 xl:grid-cols-2">
+                {props.posts.map((post: any) => (
+                  <Post
+                    id={post.id}
+                    key={post.id}
+                    title={post.title}
+                    views={post.visualizations}
+                    date={dateUtil.timeAgo(post.createdAt)}
+                    userImage={
+                      post.userProfilePhotoUrl
+                        ? process.env.NEXT_PUBLIC_BASE_DOWNLOAD_URL +
+                          post.userProfilePhotoUrl
+                        : `https://avatar.oxro.io/avatar.svg?name=${
+                            post.username ? post.username.replace(' ', '+') : ''
+                          }`
+                    }
+                    postBg={
+                      post.images[0]
+                        ? process.env.NEXT_PUBLIC_BASE_DOWNLOAD_URL +
+                          post.images[0]?.path
+                        : `https://avatar.oxro.io/avatar.svg?name=${
+                            post.title
+                              ? post.title.toUpperCase().replace(' ', '+')
+                              : ''
+                          }`
+                    }
+                  />
+                ))}
+              </div>
+            </div>
           </div>
         </div>
       </HomeContainer>
@@ -43,7 +79,7 @@ const Settings: NextPage = (props: any) => {
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  let user
+  let user, posts
 
   try {
     user = await session.getLoggedUser(context)
@@ -55,9 +91,18 @@ export async function getServerSideProps(context: GetServerSidePropsContext) {
     }
   }
 
+await api.get(
+    'posts?onlyCreatedByMe=true'
+).then((data) => {
+    posts = data?.data?.data ?? [];
+})
+  
+  console.log(posts)
+
   return {
     props: {
       user,
+      posts
     },
   }
 }
